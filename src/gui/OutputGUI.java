11e2def3f582a06658eb008f325f35b9c381afcc
bbuck108 <bbuck108@gmail.com>
2016-02-02 09:53:10 -0500

    Fix output gui

--- src/gui/OutputGUI.java
+++ src/gui/OutputGUI.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -20,6 +21,23 @@ public class OutputGUI extends JFrame{
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+	}
+
+	public OutputGUI(ArrayList<String> outputArray) {
+		setSize(240,240);
+		
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		while(true){
+			try{
+				String t_1=outputArray.get(0);
+				outputArray.remove(0);
+				JLabel lblTheHostName = new JLabel(t_1);
+				getContentPane().add(lblTheHostName, BorderLayout.CENTER);
+			}catch(IndexOutOfBoundsException e){
+				
+			}
+		}
 	}
 
 }
--- src/network/Client.java
+++ src/network/Client.java
@@ -5,6 +5,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 import gui.OutputGUI;
 
@@ -12,12 +13,16 @@ public class Client {
 	Socket socket;
 	DataInputStream in;
 	DataOutputStream out;
+	private OutputGUI outputWindow;
+	private ArrayList<String> outputArray;
 	public Client(String host) throws UnknownHostException, IOException{
 		socket = new Socket(host ,80);
 		in = new DataInputStream(socket.getInputStream());
 		out = new DataOutputStream(socket.getOutputStream());
+		outputArray = new ArrayList<String>();
+		outputWindow = new OutputGUI(outputArray);
 		while(true){
-			new OutputGUI(in.readUTF());
+			outputArray.add(in.readUTF());
 		}
 	}
 }
